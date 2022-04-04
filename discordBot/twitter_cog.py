67898bb7536b4a908b49175842c145e4b19251cc
Dickson Chan <sab3r-chan@hotmail.com>
2022-04-04 06:16:03 -0400

    2.0 - Sven Update

--- discordBot/twitter_cog.py
+++ discordBot/twitter_cog.py
@@ -2,6 +2,7 @@ import tweepy
 from discord.ext import commands
 from discord.utils import get
 
+## Work in Progress
 class twitter(commands.Cog):
     def __init__(self, client):
         self.client = client
